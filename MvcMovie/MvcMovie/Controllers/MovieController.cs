c56b1a3759c14682e3ea96ff015d3c0577161dba
quigleys <smqley@gmail.com>
2016-09-21 22:14:12 -0400

    MVC ASP.NET Example

---
+++ MvcMovie/MvcMovie/Controllers/MovieController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{ 
+    public class MovieController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        //
+        // GET: /Movie/
+
+        public ViewResult Index()
+        {
+            return View(db.Movies.ToList()); //pass list from controller to view
+        }
+
+        //
+        // GET: /Movie/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // GET: /Movie/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Movie/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(movie);
+        }
+        
+        //
+        // GET: /Movie/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movie/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movie/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Movie/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        //Method to return a view that contains an HTML form
+        public ActionResult SearchIndex(string searchString)
+        {
+            var movies = from m in db.Movies
+                         select m; //LINQ Query to select movies
+
+            if (!String.IsNullOrEmpty(searchString)) //Movie query is modified to filter based on value of the search string
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(movies);
+        }
+    }
+}
